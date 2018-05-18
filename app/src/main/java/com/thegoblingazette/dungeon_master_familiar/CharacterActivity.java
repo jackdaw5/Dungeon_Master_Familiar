791b64056c026eb6dada5099acdb24535505d8aa
jackdaw5 <s.sanazaro91@yahoo.com>
2018-05-18 15:40:04 -0500

    Initial commit

---
+++ app/src/main/java/com/thegoblingazette/dungeon_master_familiar/CharacterActivity.java
@@ -0,0 +1,17 @@
+package com.thegoblingazette.dungeon_master_familiar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character);
+    }
+}
